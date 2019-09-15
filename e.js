3e2a8c6caf515d20e3da9619ffcc45016d756185
HuongNTN <huongntn@mecorp.vn>
2019-09-15 10:40:35 +0700

    f.js

--- e.js
+++ e.js
@@ -1,16 +1,39 @@
-let h = 8;
-for (let y = 1; y <= 8; ++y) {
-    let d = '';
-    for (let x = 1; x <= 8; ++x) {
-        if (x <= y) {
-            d += ' ' + x;
+function veHinh1(h = 8) {
+    for (let y = 1; y <= h; ++y) {
+        let d = '';
+        for (let x = 1; x <= h; ++x) {
+            if (x <= y) {
+                d += ' ' + '*';
+            }
         }
+        console.log(d)
     }
-    console.log(d)
 }
+veHinh1()
 
 // *        y=1; x=1
 // * *      y=2; x=1,2
 // * * *    y=3; x=123
-// * * * *
+// * * * * =>     x<=y
 // * * * * *
+
+function veHinh2(h = 8) {
+    for (let y = 1; y <= h; ++y) {
+        let d = '';
+        for (let x = 1; x <= h; ++x) {
+            if (x >= h - y + 1) {
+                d += ' ' + '*';
+            }
+            else {
+                d += '  '
+            }
+        }
+        console.log(d)
+    }
+}
+veHinh2()
+//         * y=1; x=5
+//       * * y=2; x=54
+//     * * * y=3; x=543
+//   * * * * y=4; x=5432
+// * * * * * =>    x >= h-y+1
---
+++ f.js
@@ -0,0 +1,9 @@
+/**
+ * cho start = 0 ; end = 10
+ * Viet function thuc hien cac chuc nang sau:
+ * 1. In ra cac so chan
+ * 2. In ra cac so le
+ * 3. In ra so chia 4 dư 3
+ * 4. In ra so chia 4 dư 2
+ *
+ */
